e5b2f35323b655c672d21f0b7c1ba6811e458aeb
Ando <a.r.north23@gmail.com>
2021-12-21 01:48:46 +0000

    1) added userid cookie usage for interface to db 2) updated error handling for time stamps 3) corrected error handling return true on error and false on success 4) added listitem timestamp interface to add to db

--- src/php/addItemToDB-inc.php
+++ src/php/addItemToDB-inc.php
@@ -1,14 +1,24 @@
 <?php
-/* error handling */
-function emptyListItem($userid, $itemName, $tag, $desc){
+/* error handling - return true if error */
+function emptyListItem($userid, $itemName, $tag, $desc, $timestamps){
     $result;
     if(empty($userid) || empty($itemName) ||
     empty($tag) || empty($desc) ||
     empty($timestamps)) {
+        echo "$userid, $itemName, $tag, $desc, $timestamps\n";
         $result = true;
     }
     else {
-        $result = false;
+        foreach($timestamps as $timestamp){
+            foreach($timestamp as $stamp){
+                if(empty($stamp)){
+                    $result = true;
+                }
+                else{
+                    $result = false;
+                }
+            }
+        }
     }
     return $result;
 }
@@ -16,45 +26,53 @@ function userIdExists($conn, $userid){
     $sql = "SELECT * FROM list_users WHERE userId = ?;";
     $stmt = mysqli_stmt_init($conn); // use prepared statements to make query
     if(!mysqli_stmt_prepare($stmt, $sql)) {
-        return false;
+        return true;
     }
     mysqli_stmt_bind_param($stmt, "i", $userid); // i = int
     mysqli_stmt_execute($stmt);
     $resultData = mysqli_stmt_get_result($stmt);
     if(mysqli_fetch_assoc($resultData)){
         mysqli_stmt_close($stmt);
-        return true;
+        return false;
     }
     else{
-        return false;
+        return true;
     }
 }
 // does list item exist in DB
-function listItemExists($userid, $itemName, $tag, $desc){
-    $sql = "SELECT * FROM list_items WHERE (userId, itemName, Tag, Description) = (?,?,?,?);";
+function listItemExists($conn, $userid, $itemName, $tag, $desc){
+    $sql = "SELECT * FROM list_items WHERE (userId, itemName, Tag, Description) = (?, ?, ?, ?);";
     $stmt = mysqli_stmt_init($conn);
     if(!mysqli_stmt_prepare($stmt, $sql)) {
-        return false;
+        return true;
     }
     mysqli_stmt_bind_param($stmt, "isss", $userid, $itemName, $tag, $desc); // i = int
     mysqli_stmt_execute($stmt);
     $resultData = mysqli_stmt_get_result($stmt);
     if(mysqli_fetch_assoc($resultData)){
         mysqli_stmt_close($stmt);
-        return true;
+        return false;
     }
     else{
-        return false;
+        return true;
     }
 }
 // does time stamp exist in DB
-function timeStampExists($userid, $itemName, $tag, $desc, $startTime, $endTime){
-    $sql = "SELECT * FROM list_items WHERE (userId, itemName, Tag, Description, startTime, endTime) = (?,?,?,?,?,?);";
+function timeStampExists($conn, $userid, $itemName, $tag, $desc, $startTime, $endTime){
+    // STR_TO_DATE(?, '%Y-%m-%dT%H:%i:%sZ'), CAST(? AS DATETIME)
+    $sql = "SELECT * FROM list_timestamps WHERE (userId, itemName, Tag, Description, startTime, endTime) = (?, ?, ?, ?, ?, ?);";
     $stmt = mysqli_stmt_init($conn);
     if(!mysqli_stmt_prepare($stmt, $sql)) {
-        return false;
+        return true;
     }
-    mysqli_stmt_bind_param($stmt, "isssss", $userid, $itemName, $tag, $desc, $startTime, $endTime);
+    $format = 'Y-m-d\TH:i:s\Z'; // ISO8601 format
+    //$format = 'Y-m-d H:i:s'; // mysql format
+    //date($format, strtotime($string)), DateTime::createFromFormat($format, $string);
+    $stime = strtotime($startTime);
+    $etime = strtotime($endTime);
+    $sstime = date($format, $stime);
+    $eetime = date($format, $etime);
+    mysqli_stmt_bind_param($stmt, "isssss", $userid, $itemName, $tag, $desc, $sstime, $eetime);
     mysqli_stmt_execute($stmt);
     $resultData = mysqli_stmt_get_result($stmt);
     if(mysqli_fetch_assoc($resultData)){
@@ -67,27 +85,35 @@ function timeStampExists($userid, $itemName, $tag, $desc, $startTime, $endTime){
 }
 /* interact with DB */
 // add list item to DB
-function addListItemToDb($userid, $itemName, $tag, $desc){
+function addListItemToDb($conn, $userid, $itemName, $tag, $desc){
     $sql = "INSERT INTO list_items (userId, itemName, Tag, Description) VALUES (?,?,?,?);";
     $stmt = mysqli_stmt_init($conn);
     if(!mysqli_stmt_prepare($stmt, $sql)) {
-        return false;
+        return true;
     }
     mysqli_stmt_bind_param($stmt, "isss", $userid, $itemName, $tag, $desc); // ssss = string string string string
     mysqli_stmt_execute($stmt);
     mysqli_stmt_close($stmt);
-    return true;
+    return false;
 }
 // add time stamp to DB
-function addTimeStampToDb($userid, $itemName, $tag, $desc, $startTime, $endTime){
-    $sql = "INSERT INTO list_items (userId, itemName, Tag, Description, startTime, endTime) VALUES (?,?,?,?,?,?);";
+function addTimeStampToDb($conn, $userid, $itemName, $tag, $desc, $startTime, $endTime){
+    // STR_TO_DATE(?, '%Y-%m-%dT%H:%i:%sZ'), CAST(? AS DATETIME)
+    $sql = "INSERT INTO list_timestamps (userId, itemName, Tag, Description, startTime, endTime) VALUES (?,?,?,?,?,?);";
     $stmt = mysqli_stmt_init($conn);
     if(!mysqli_stmt_prepare($stmt, $sql)) {
-        return false;
+        return true;
     }
-    mysqli_stmt_bind_param($stmt, "isssss", $userid, $itemName, $tag, $desc, $startTime, $endTime); // ssss = string string string string
+    $format = 'Y-m-d\TH:i:s\Z'; // ISO8601 format
+    $format = 'Y-m-d H:i:s'; // mysql format
+    //date($format, strtotime($string)), DateTime::createFromFormat($format, $string);
+    $stime = strtotime($startTime);
+    $etime = strtotime($endTime);
+    $sstime = date($format, $stime);
+    $eetime = date($format, $etime);
+    mysqli_stmt_bind_param($stmt, "isssss", $userid, $itemName, $tag, $desc, $sstime, $eetime); // ssss = string string string string
     mysqli_stmt_execute($stmt);
     mysqli_stmt_close($stmt);
-    return true;
+    return false;
 }
 ?>
--- src/php/addItemToDB.php
+++ src/php/addItemToDB.php
@@ -3,29 +3,58 @@
 *  brief: from POST request make query to add list-item to DB
 */
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
+    session_start();
     // initialise variables
-    $userid=$_POST['userid'];
-    $itemName=$_POST['itemName'];
-    $tag=$_POST['tag'];
-    $desc=$_POST['desc'];
-    $timestamps=$_POST['timestamps'];
+    $userid=$_COOKIE['userid'];
+    if(isset($userid)){
+        $userid=$_SESSION['userid'];
+    }
+    $data = json_decode(file_get_contents('php://input'), true);
+    $itemName=$data['item-name'];
+    $tag=$data['tag'];
+    $desc=$data['item-desc'];
+    $timestamps=$data['time-stamps'];
     require_once 'addItemToDB-inc.php';
-    // error handling for data
-    if(emptyInputSignup($userid, $itemName, $tag, $desc, $timestamps) !== false){
+    // error handling for data, !== false, if anything besides false then go into braces
+    if(emptyListItem($userid, $itemName, $tag, $desc, $timestamps) !== false){
+        http_response_code(400);
         exit("error: bad request");
     }
-    if(userIdExists !== false){
+    require_once 'dbh-inc.php';
+    if(userIdExists($conn, $userid) !== false){
+        http_response_code(400);
         exit("error: userid doesnt exist");
     }
-    require_once 'dbh-inc.php';
-    if(listItemExists($userid, $itemName, $tag, $desc) !== false){
-        if(addListItemToDb($userid, $itemName, $tag, $desc) !== false){
+    // add list item
+    if(listItemExists($conn, $userid, $itemName, $tag, $desc) !== false){
+        // list item exists move on
+    }
+    else{
+        // attempt to add a list item
+        if(addListItemToDb($conn, $userid, $itemName, $tag, $desc) !== false){
+            http_response_code(500);
             exit("error: listItem could not be added");
         }
     }
-    // check whether time stamp exists
-        // add time stamps to db
+    // add time stamp
+    foreach($timestamps as $timestamp){
+        $startTime = $timestamp['startTime'];
+        $endTime = $timestamp['endTime'];
+        if(timeStampExists($conn, $userid, $itemName, $tag, $desc, $startTime, $endTime) !== false){
+            http_response_code(400);
+            exit("error: timeStamp already exists");
+        }
+        if(addTimeStampToDb($conn, $userid, $itemName, $tag, $desc, $startTime, $endTime) !== false){
+            http_response_code(400);
+            exit("error: timeStamp could not be added");
+        }
+        else{
+            http_response_code(201);
+            exit();
+        }
+    }
 } else {
-    exit("error: bad request");
+    http_response_code(400);
+    exit("error: bad request - not POST");
 }
 ?>
